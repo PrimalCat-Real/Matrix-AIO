08e858435f6156b1d848d13a17fc8047a8b6d13b
PrimalCat-Real <romanenko.denis2003@gmail.com>
2023-01-28 09:55:16 +0200

    change default search type

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -58,7 +58,7 @@ export default {
             term: '',
             responseData: [],
             formattedData: [],
-            isGiveaways: false
+            isGiveaways: true
         }
     },
     computed: {
